9b93287b108705125a89d05d6160a8317b263c71
Vlad-Cristian Prisacariu <vcprisacariu1@sheffield.ac.uk>
2022-03-15 21:50:37 +0000

    Revert "Working on dynamic programming problems from lesson"
    This reverts commit 2f4f04759c4014cc6aec46bc9df5dd3ebd7999d7.

--- knapsack/KnapsackInstance.java
+++ knapsack/KnapsackInstance.java
@@ -21,23 +21,28 @@ public class KnapsackInstance {
 
     public int CalcLowerBound(){
         int lower_bound = 0;
+
         for (int i = 0; i<item_choices.length; i++) {
-            if (item_choices[i] == true) {
+            if (item_choices[i] == ) {
                 C -= items[i].weight;
                 lower_bound += items[i].weight;
             }
         }
+
         for (int i = 0; i<items.length; i++){
             if ((items[i].weight < C) && (item_choices[i] == true)) {
                 item_choices[i] = true;
                 C -= items[i].weight;
                 lower_bound += items[i].weight;
             }
-        }   
-        return 0;
+        }
+
+        
     }
+
+
     public int CalcUpperBound(){
-        return 0;
+
     }
 
 
